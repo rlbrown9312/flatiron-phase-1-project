a65af6fd23ae1b60927e9947dea860cff9c74056
rlbrown9312 <richard.brown0807@gmail.com>
2022-10-09 14:35:12 -0500

    Rendering characters to page

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ fetch("http://localhost:3000/characters")
     })
 
 
+const forEachCharcter = (character) => {
+    character.forEach(addCharactersToPage)
+}
 
+const addCharactersToPage = (character) => {
+
+    const scoobyGangContainer = document.querySelector("#scooby-gang_container");
+    const img = document.createElement("img");
+    img.src = character.image
+    img.addEventListener("click", () => {
+        renderCharacterDetails(character) 
+           
+    })
+    scoobyGangContainer.append(img);
+}
     
